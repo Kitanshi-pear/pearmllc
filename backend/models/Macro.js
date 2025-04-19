4beef7066e25f55dd0b6d435d69fd79551610c43
Kitanshi-pear <kitanshi.singh@pearmediallc.com>
2025-04-20 02:03:48 +0530

    server

--- backend/models/Macro.js
+++ backend/models/Macro.js
@@ -1,9 +1,16 @@
 module.exports = (sequelize, DataTypes) => {
   const Macro = sequelize.define("Macro", {
     click_id: {
-      type: DataTypes.STRING,
-      allowNull: false
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Clicks',
+        key: 'id',
+      },
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
     },
+    
     ...Object.fromEntries(
       Array.from({ length: 25 }, (_, i) => [`sub${i + 1}`, DataTypes.STRING])
     )
